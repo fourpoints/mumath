f87a57c55e3844667259d18f2d70f40a8f939446
fourpoints <auronorth@gmail.com>
2018-05-31 19:36:15 +0200

    update/clear context

--- mumath/Context/ContextManager.py
+++ mumath/Context/ContextManager.py
@@ -55,9 +55,9 @@ SYMBOL_MAP = {
     '\(' : (MAction, ("NONE", {}, "OPENPREV", None)),
     '\]' : (MAction, ("NONE", {}, "CLOSEPREV", None)),
     r'\\': (MAction, ("NONE", {}, "NEWLINE", "")),
+    '\{' : (MAction, ("mo", {"fence": "true"}, "OPEN", "{")),
+    '\}' : (MAction, ("mo", {"fence": "true"}, "CLOSE", "}")),
 
-    '\{' : (MObject, ("mo", {}, "open", "{")),
-    '\}' : (MObject, ("mo", {}, "close", "}")),
     '\.' : (MObject, ("mo", {}, "bracket", "")), #???
     '\|' : (MObject, ("mo", {}, "bracket", "&Vert;")),
     '\,' : (MObject, ("mo", {}, "InvisibleComma", "&ic;")),
@@ -117,3 +117,26 @@ def GET_ACTION(key):
         tag, attr, type_, text = mAction
         return MAction(tag, attr.copy(), type_, text)
     return None
+
+
+def UPDATE_CONTEXT(CONTEXT):
+    CONTEXT_MAP.update(CONTEXT)
+
+def UPDATE_UNICODE(UNICODE):
+    UNICODE_MAP.update(UNICODE)
+
+def UPDATE_ACTION(ACTION):
+    ACTION_MAP.update(ACTION)
+
+
+def CLEAR_CONTEXT(CONTEXT):
+    for key in CONTEXT:
+        CONTEXT_MAP.pop(key, None)
+
+def CLEAR_UNICODE(UNICODE):
+    for key in UNICODE:
+        UNICODE_MAP.pop(key, None)
+
+def CLEAR_ACTION(ACTION):
+    for key in ACTION:
+        ACTION_MAP.pop(key, None)
